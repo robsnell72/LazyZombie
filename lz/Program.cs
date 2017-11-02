18996e831e0df2bed3b87f171b2e98bc4b5ccc78
robsnell72 <rob.snell.1@gmail.com>
2017-11-02 08:55:59 -0400

    initial check-in

---
+++ lz/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int x=4;x<=5000;x++)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    for (int j = 1; j <= 10; j++)
+                    {
+                        for (int k = 1; k <= 10; k++)
+                        {
+                            for (int l = 1; l <= 10; l++)
+                            {
+                                if (Math.Pow(i, 2) + Math.Pow(j, 2) + Math.Pow(k, 2) + Math.Pow(l, 2) == x)
+                                {
+                                    String msg = string.Format("x:{4} i:{0} j:{1} k:{2} l:{3}", i, j, k, l, x);
+                                    Console.WriteLine(msg);
+                                    result.AppendLine(msg);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            File.AppendAllText("cara.txt", result.ToString());
+            Console.ReadLine();
+        }
+    }
+}
